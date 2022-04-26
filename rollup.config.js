dda6b1593265c5a4bf11731cb599fdeaa4455990
Pavel Lukasenko <pavel@kenzap.com>
2022-04-26 12:30:23 +0700

    rollup.config update

--- rollup.config.js
+++ rollup.config.js
@@ -7,9 +7,7 @@ import babel from "@rollup/plugin-babel"
 import commonjs from "@rollup/plugin-commonjs"
 import copy from 'rollup-plugin-copy'
 import fs from 'fs';
-
-// TODO - remove these two
-// import serve from 'rollup-plugin-serve'
+import replaceHtmlVars from 'rollup-plugin-replace-html-vars';
 import livereload from 'rollup-plugin-livereload'
 
 // get build mode
@@ -64,25 +62,8 @@ const scriptOutputs = scriptPaths.reduce((files, file) => {
 	const inputPath = scriptInputs[file]
 	const parts = inputPath.split("/")
 	const pathIndex = parts.indexOf("index.js") - 1
-	// const scriptName = parts.indexOf("scripts.js") - 1
+
 	const outputPath = parts.slice(pathIndex).join("/")
-
-	// find working folders only
-	// if(outputPath.indexOf("home/") != -1 && outputPath.indexOf("_/") != -1){
-	// 	workingFolders.push(parts[parts.length-2]);
-	// }
-
-	// // homepage goes to root
-	// if(outputPath.indexOf("home/") != -1){
-
-	// 	return { [file]: absolutePath(targetFolder + parts[parts.length-1]), ...files }
-
-	// // any other page ordinary route
-	// }else{
-
-		// return { [file]: absolutePath(targetFolder + outputPath), ...files }
-	// }
-
 
 	workingFolders.push(parts[parts.length-2]);
 	return { [file]: absolutePath(targetFolder + outputPath), ...files }
@@ -98,19 +79,13 @@ const scriptOutputs = scriptPaths.reduce((files, file) => {
 
 if(prodEnv){
 	
-	// flush public folder
-	// if (fs.existsSync(targetFolder)) fs.rmdirSync(targetFolder, { recursive: true });
-
 	// set locales folder
-	// fs.mkdirSync( targetFolder );
-	fs.mkdirSync( targetFolder + 'locales/' );
+	if (!fs.existsSync( targetFolder + 'locales/' )){ fs.mkdirSync( targetFolder + 'locales/' ); }
 	fs.writeFileSync( targetFolder + 'locales/default.json', '{ "language": "default", "texts": {} }', { flag:'w' } ); // set defaults
 
-	// if (!fs.existsSync( targetFolder + 'locales/' )){ fs.mkdirSync( targetFolder + 'locales/' ); } // create locales folder if not exists yet
-	// if (fs.existsSync( targetFolder + 'locales/default.json' )){ fs.rmSync( targetFolder + 'locales/default.json', { recursive: true }); } // remove previous version 
-	
 	let scriptFilesAll = glob.sync(absolutePath("src/**/*.js"))
-	scriptFilesAll.reduce((files, input) => { i18n(input); });
+	scriptFilesAll.forEach((input) => { i18n(input); });
+
 	function i18n(key){ // export i18n
 
 		// get current strings
@@ -121,34 +96,18 @@ if(prodEnv){
 		if (fs.existsSync( key )){
 			
 			var temp = fs.readFileSync( key, {encoding:'utf8', flag:'r'});
-			var regex = new RegExp(/__\((.*?)\)/g);
+			var q1 = new RegExp(/__\('(.*?)'/g);
+			var q2 = new RegExp(/__\("(.*?)"/g);
 			var result;
-			while (result = regex.exec(temp)) {
 
-				var string = result[1].replace(/\"/g, '','g').replace(/'/g, '');
-				defJS['texts'][string] = string;
-			}
+			while (result = q1.exec(temp)) { defJS['texts'][result[1]] = result[1]; }
+			while (result = q2.exec(temp)) { defJS['texts'][result[1]] = result[1]; }
 
 			// write default locale
 			fs.writeFileSync( targetFolder + 'locales/default.json', JSON.stringify(defJS) ); 
 		}
 	}
 }
-
-// creates predefined index.html files for browser navigation
-// function createIndexes(key){
-
-// 	let keyF = key.replace('.js','.html');
-// 	keyF = keyF.replace('home/','');
-
-// 	fs.copyFile('src/_/_index.html', targetFolder + keyF, function (err) {
-// 		if (err) throw err;
-// 		// console.log('Error creating '+targetFolder + keyF+' index file, make sure that template index file exists in src/_/_index.html. Error: ' + err);
-// 	});
-
-// 	// public/home is now moved to root folder, removing instead TODO results in conflicts
-// 	// if (fs.existsSync(targetFolder + 'home')) fs.rmdirSync(targetFolder + 'home', { recursive: true });
-// }
 
 const bundles = scriptPaths.map((key) => {
 
@@ -165,18 +124,24 @@ const bundles = scriptPaths.map((key) => {
 		}),   
 		copy({
 			targets: [
-				{ src: ['public/home/*'], dest: 'public' },
-				// { src: ['src/**/*', '!src/_/*'], dest: 'public' },
+				{ src: ['public/home/*'], dest: 'public' }
 			],
 			// flatten: false
-		}),
-		// createIndexes(key),
+		})
 	]
 
 	// plugin list for production mode
 	if (prodEnv) {
 
 		plugins.push(terserPlugin)
+		plugins.push(
+			replaceHtmlVars({
+				files: 'public/*/index.html',
+				from: /\${timestamp}/g,
+				to: new Date().getTime(),
+			})
+		)
+		
 		sourcemap = false
 	// plugin list development mode
 	}else{
